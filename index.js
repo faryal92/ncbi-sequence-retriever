360540119e814675f7602d0992a003b086252442
Faryal <64413984+faryal92@users.noreply.github.com>
2020-11-30 21:45:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const NcbiSeqRetriever = require("./src/NcbiSeqRetriever");
-const retriever = new NcbiSeqRetriever();
+const retriever = new NcbiSeqRetriever(https://github.com/faryal92/ncbi-sequence-retriever/blob/a362a576e144568baf03fb990fd6fc01f6117191/package.json#L1);
 
 module.exports = retriever;
